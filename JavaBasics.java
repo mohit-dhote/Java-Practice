8ab5ac6784186afbe2382076f6ca64d3eb3f6553
Mohit Dhote <89855871+mohit-dhote@users.noreply.github.com>
2024-02-26 14:10:36 +0530

    Delete JavaBasics.java

--- JavaBasics.java
+++
@@ -1,7 +0,0 @@
-public class JavaBasics { 
-public static void main(String args[]) { 
-     System.out.println("Hello world");
-     System.out.print("1,2,3,4\n");
-     System.out.print("1,2,3,4");
-}
-}
